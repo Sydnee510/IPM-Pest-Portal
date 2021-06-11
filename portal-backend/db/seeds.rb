54f5228cb0a5a1a9ad88b27285678ef641e1a800
sydnee510 <sbsydnee161416@gmail.com>
2021-06-11 06:36:56 -0700

    loaded rest of seed data from .csv files -no associations

--- portal-backend/db/seeds.rb
+++ portal-backend/db/seeds.rb
@@ -7,8 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
+
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'locations.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+puts csv_text
 csv.each do |row|
     t = Location.new
     t.name = row['name']
@@ -16,5 +18,39 @@ csv.each do |row|
     puts "#{t.name}, #{t.city} saved"
     t.save
   end
+ puts "There are now #{Location.count} rows in the locations table"
 
-  puts "There are now #{Location.count} rows in the locations table"
+
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'work_orders.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+puts csv_text
+csv.each do |row|
+    t = WorkOrder.new
+    t.user_id = row['user_id']
+    t.location_id = row['location_id']
+    t.time = row['time']
+    t.duration = row['duration']
+    t.price = row['price']
+    puts "#{t.user_id}, #{t.location_id}, #{t.time}, #{t.duration}, #{t.price}, saved"
+    t.save
+  end
+ puts "There are now #{WorkOrder.count} rows in the work orders table"
+
+
+ csv_text = File.read(Rails.root.join('lib', 'seeds', 'users.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+puts csv_text
+csv.each do |row|
+    t = User.new
+    t.name = row['name']
+    puts "#{t.name} saved"
+    t.save
+  end
+ puts "There are now #{User.count} rows in the users table"
+
+ 
+ 
+ 
+ 
