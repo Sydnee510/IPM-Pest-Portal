d619111483df5b083df6fb1365283d5ca77c3ee1
sydnee510 <sbsydnee161416@gmail.com>
2021-06-10 22:04:39 -0700

    create location forms and views with controller actions

--- portal-backend/app/controllers/locations_controller.rb
+++ portal-backend/app/controllers/locations_controller.rb
@@ -1,2 +1,46 @@
 class LocationsController < ApplicationController
+    # before_action :authenticate_user!
+   # before_action :set_myth
+   def index
+    @locations = Location.all 
+end
+    def show 
+        @location = Location.find(params[:id])
+    end 
+    def new 
+        @location = Location.new
+    end 
+    def edit 
+        @location =  Location.find(params[:id])
+    end 
+    def create 
+      #  @location = Location.find(params[:id])
+        @location = Location.new(location_params)
+        if @location.save 
+            redirect_to location_path(@location), notice: "Location Successfully Created"
+        else 
+            render 'new'
+        end 
+    end 
+    def destroy 
+        @location = Location.find(params[:id]).destroy
+        redirect_to locations_path(@locations)
+    end 
+    def update 
+        @location = Location.find(params[:id])
+        if @location.update(location_params)
+            redirect_to location_path(@location), notice: "location Successfully Updated" 
+        else 
+            render :edit 
+        end
+    end
+
+    private 
+    def set_location
+        @location = Location.find(params[:location_id])
+    end 
+
+    def location_params
+        params.require(:location).permit(:name, :city)
+    end 
 end
--- portal-backend/app/models/location.rb
+++ portal-backend/app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ApplicationRecord
+    has_many :work_orders
+    has_many :users, through: :work_orders
+    
+    validates :name, presence: true
+    validates :city, presence: true
+
 end
